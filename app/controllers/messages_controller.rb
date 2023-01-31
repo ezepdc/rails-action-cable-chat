bec951bcc74ab3b23eee6931e6e03df8b7171500
Ezequiel <epuyosdacosta@gmail.com>
2023-01-31 14:53:45 +0100

    Auto scroll down

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,16 +5,13 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
 
-    if @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        # "Yeah!"
-        render_to_string(partial: "message", locals: { message: @message })
-      )
-      redirect_to chatroom_path(@chatroom)
-    else
-      render "chatroom/show", status: :unprocessable_entity
-    end
+    @message.save
+    ChatroomChannel.broadcast_to(
+      @chatroom,
+      # "Yeah!"
+      render_to_string(partial: "message", locals: { message: @message })
+    )
+    head :ok
   end
 
   private
