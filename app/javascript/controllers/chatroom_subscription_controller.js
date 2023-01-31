218e79d8590d510234c74792812a13f928a2c1ad
Ezequiel <epuyosdacosta@gmail.com>
2023-01-31 10:59:26 +0100

    Connect Stimulus controller and view

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import {createConsumer} from "@rails/actioncable"
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    // console.log(this.chatroomIdValue)
+    // console.log(this.messagesTarget)
+    createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue}
+    )
+  }
+}
