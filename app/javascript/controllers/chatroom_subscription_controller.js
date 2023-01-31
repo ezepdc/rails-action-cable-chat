4ece3131cb3fae86829a25787d2b8b7865b28722
Ezequiel <epuyosdacosta@gmail.com>
2023-01-31 15:14:56 +0100

    Unsuscribe from channel

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,13 +7,18 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
-    // console.log(this.chatroomIdValue)
+    console.log(`Connecting to the ActionCable channel with id ${this.chatroomIdValue}`)
     // console.log(this.messagesTarget)
-    createConsumer().subscriptions.create(
+    this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue},
       // { received: data => console.log(data) }
       { received: data => this.#insertMessageAndScrollDown(data) }
     )
+  }
+
+  disconnect() {
+    console.log(`Disconnecting from the channel...`)
+    this.channel.unsubscribe()
   }
 
   resetForm(event) {
